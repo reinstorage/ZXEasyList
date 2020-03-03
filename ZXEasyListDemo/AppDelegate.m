b60a8697210f6c081a01d62a60115e09b9ddde25
Shang Zhenxing <zxingshawn@outlook.com>
2020-03-03 16:00:34 +0800

    Revert "测试哈哈"
    This reverts commit df9ff01874ed95a598845accc6e72251c1f39f3e.

--- ZXEasyListDemo/AppDelegate.m
+++ ZXEasyListDemo/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //测试哈哈
+    //测试
     return YES;
 }
 
